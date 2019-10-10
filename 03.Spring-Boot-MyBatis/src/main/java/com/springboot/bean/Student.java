a31ca61a29ca09fdb6b6d69baebd1df2326d5164
lansheng <lansheng@forchange.tech>
2019-10-10 17:52:17 +0800

    update

--- 03.Spring-Boot-MyBatis/src/main/java/com/springboot/bean/Student.java
+++ 03.Spring-Boot-MyBatis/src/main/java/com/springboot/bean/Student.java
@@ -8,24 +8,29 @@ public class Student implements Serializable{
 	private String sno;
 	private String name;
 	private String sex;
+
 	public String getSno() {
 		return sno;
 	}
+
 	public void setSno(String sno) {
 		this.sno = sno;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getSex() {
 		return sex;
 	}
+
 	public void setSex(String sex) {
 		this.sex = sex;
 	}
-	
-	
+
 }
